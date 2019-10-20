d9e553ef79eb0c05f49674eb8e10f08e46980962
Lindsay <lindsay.lbm@gmail.com>
2019-10-20 11:25:22 -0500

    Bamazon Supervisor asking initial inquirer prompt

---
+++ bamazonSupervisor.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer")
+
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Inspire1298*",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    inquirerFunction();
+
+})
+
+function inquirerFunction() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "supervisorOptions",
+            message: "Please choose from the options below:",
+            choices: ["View Product Sales by Department", "Create New Department"]
+        }
+    ]).then(function (userList) {
+        console.log(userList.supervisorOptions)
+    })
+}
